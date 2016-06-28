78faeecc1c8df8b0825bb3bbcf9fb8d0446c61a4
Marcos Mirabent <mmira031@fiu.edu>
2016-06-28 04:47:22 +0000

    Added homemade i2c library because raisins

--- libi2c.h
+++ libi2c.h
@@ -5,6 +5,16 @@
 #ifndef MM_I2C_H
 #define MM_I2C_H
 
-int set_slave(int fd, int address);
+#include <stdint.h>
+#include <stddef.h>
+
+extern uint8_t i2c_slave_addr;
+
+int open_i2c(int bus);
+int set_slave(int fd, uint8_t address);
+int write_byte(int fd, uint8_t reg, uint8_t byte);
+int write_bytes(int fd, uint8_t reg, uint8_t* bytes, size_t count);
+int read_byte(int fd, uint8_t reg, uint8_t* byte);
+int read_bytes(int fd, uint8_t reg, uint8_t* bytes, size_t num);
 
 #endif
