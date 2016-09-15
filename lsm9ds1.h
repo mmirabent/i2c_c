42fc7469943a57561c700e5aba9476e77a035df6
Marcos Mirabent <mmira031@fiu.edu>
2016-09-15 21:40:42 +0000

    Removed float struct

--- lsm9ds1.h
+++ lsm9ds1.h
@@ -4,12 +4,6 @@
 #include <stdint.h>
 #include "registers.h"
 
-/* These are the values for the full scale settings for Gyroscope,
- * Accelerometer and Magnetometer in that order */
-#define FS_G_VAL 1
-#define FS_XL_VAL 2
-#define FS_M_VAL 2
-
 struct g_data {
   int16_t x;
   int16_t y;
@@ -37,21 +31,6 @@ struct m_data {
   int16_t z;
 };
 
-struct ga_data_float {
-    float gx;
-    float gy;
-    float gz;
-    float ax;
-    float ay;
-    float az;
-};
-
-struct m_data_float {
-    float x;
-    float y;
-    float z;
-};
-
 int set_hpf_reference(int id, int ref);
 int set_odr(int id, int odr);
 int set_fsg(int id, int fs);
@@ -63,7 +42,7 @@ int init_sensor(int fd);
 void get_gyro(int fd, struct g_data* data);
 void get_accel(int fd, struct a_data* data);
 void get_mag(int fd, struct m_data* data);
-void get_gyro_accel(int fd, struct ga_data* data, struct ga_data_float* fdata);
+void get_gyro_accel(int fd, struct ga_data* data);
 
 #endif
 
